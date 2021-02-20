e9708bde243942e370e5069a50173929605e0ddf
pratiksha <pratiksha98shinde@gmail.com>
2021-02-20 09:17:31 +0530

    product customer entity added

---
+++ OnlineShoppingPC/src/test/java/com/onlineshopping/OnlineShoppingPC/OnlineShoppingPcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineshopping.OnlineShoppingPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
