1c63578b9229652f1a599353c8d2775d0d9ca7bb
aashu <aashu.villa123@gmail.com>
2021-02-20 08:20:11 +0530

    customer_entity_folder

---
+++ Customer Entity- OSC Project/OnlineShopping/src/main/java/onlineshopping/controller/CustomerController.java
@@ -0,0 +1,64 @@
+package onlineshopping.controller;
+
+import java.util.ArrayList;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import onlineshopping.bean.Customer;
+import onlineshopping.service.CustomerService;
+
+@RestController
+@RequestMapping(value="customer")
+public class CustomerController {
+	@Autowired
+	CustomerService cs;
+	
+	
+	@GetMapping(value = "getAllCustomerData",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Customer> getAllCustomerDetailsFromSpringData(){
+			return cs.getAllCustomerFormSpringData();
+	}
+	
+	
+
+	
+	@PostMapping(value = "storeCustomerData",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String storeCustomerSpringData(@RequestBody Customer cc) {
+		return cs.storeCustomerSpringData(cc);
+	}
+	
+	
+
+	
+	@PutMapping(value = "updateCustomerData",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String updateCustomerSpringData(@RequestBody Customer cc) {
+			return cs.updateCustomerSpringData(cc);
+	}
+	
+	
+
+
+	@DeleteMapping(value = "deleteCustomerData/{custid}")
+	public String deleteCustomerSpringData(@PathVariable("custid") int custid) {
+			return cs.deleteCustomerSpringData(custid);
+	}
+	
+	
+
+}
