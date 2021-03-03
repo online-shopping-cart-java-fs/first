9d1d4ebd0ca013c910d221c69b28886045dcb914
aashu <aashu.villa123@gmail.com>
2021-03-03 22:28:13 +0530

    updation in backend

--- onlineshopping/src/main/java/onlineshopping/controller/AdminController.java
+++ onlineshopping/src/main/java/onlineshopping/controller/AdminController.java
@@ -1,6 +1,7 @@
 package onlineshopping.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import onlineshopping.bean.Address;
+import onlineshopping.bean.Customer;
 import onlineshopping.bean.Item;
 import onlineshopping.bean.Product;
+import onlineshopping.service.AddressService;
+import onlineshopping.service.CustomerService;
 import onlineshopping.service.ItemService;
+import onlineshopping.service.JoinService;
 import onlineshopping.service.ProductItemService;
 import onlineshopping.service.ProductService;
 
@@ -32,6 +38,18 @@ public class AdminController {
 	
 	@Autowired
 	ProductItemService productItemService;
+	
+	@Autowired
+	AddressService addressService;
+	
+	@Autowired
+	JoinService ccs;
+	
+	@Autowired
+	CustomerService cs;
+	
+	
+	
 	
 	//dashboard related operations
 	
@@ -65,6 +83,9 @@ public class AdminController {
 		List<Object[]> productItemDetails = productItemService.getAllProductsByCategory(itemid);
 		return ResponseEntity.status(200).body(productItemDetails);
 	}
+	
+	
+	
 		
 	@PostMapping(value = "storeProductData",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.TEXT_PLAIN_VALUE)
 	public String storeProductData(@RequestBody Product p) {
@@ -80,5 +101,40 @@ public class AdminController {
 	public String deleteProductData(@PathVariable("pid") int pid ) {
 		return productService.deleteProduct(pid);
 	}
+	
+	
+	@GetMapping(value = "getAllCustomerData",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Customer> getAllCustomerDetailsFromSpringData(){
+			return cs.getAllCustomerFormSpringData();
+	}
+	
+	@GetMapping(value = "getCustomerData/{custid}")
+	public Optional<Customer> getCustomerSpringData(@PathVariable("custid") int custid) {
+			return cs.getCustomerSpringData(custid);
+	}
+	
+	@DeleteMapping(value = "deleteCustomerData/{custid}")
+	public String deleteCustomerSpringData(@PathVariable("custid") int custid) {
+			return cs.deleteCustomerSpringData(custid);
+	}
+	
+	
+	@GetMapping(value = "creditinfo/{custid}")
+	public ResponseEntity<List<Object[]>> getCustomerCreditInfo(@PathVariable("custid") int custid){
+					List<Object[]> details = ccs.getCustomerCreditNumDetails(custid);
+				
+					return ResponseEntity.status(200).body(details);
+					
+	}
+	@GetMapping(value = "addressinfo/{custid}")
+	public ResponseEntity<List<Object[]>> getCustomerAddressInfo(@PathVariable("custid") int custid){
+					List<Object[]> details = ccs.getCustomerAddressDetails(custid);
+					
+					return ResponseEntity.status(200).body(details);
+					
+	}
+
+	
+
 	
 }
